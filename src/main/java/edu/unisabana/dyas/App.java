91d11ef1ff2c167d790ab9ad68f91a049428630b
Zadcry <zadcry@gmail.com>
2024-09-27 19:06:22 -0500

    Commit Inicial

---
+++ src/main/java/edu/unisabana/dyas/App.java
@@ -0,0 +1,13 @@
+package edu.unisabana.dyas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
