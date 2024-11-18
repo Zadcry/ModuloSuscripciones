66548425da170dd4bcaf14b67fdf4acb400b3343
AGGE82 <125081820+AGGE82@users.noreply.github.com>
2024-11-17 23:34:47 -0500

    Update DatabaseConnection.java

--- src/main/java/edu/unisabana/dyas/modSuscripciones/DatabaseConnection.java
+++ src/main/java/edu/unisabana/dyas/modSuscripciones/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/contentdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Zoroark";
+    private static final String URL = "jdbc:mysql://localhost:3306/DATABASE";
+    private static final String USER = "USER";
+    private static final String PASSWORD = "PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
